bf04e6a96331ae9d8e4a2854ea625323678a474f
Wangshuo <10511026@qq.com>
2018-07-30 13:21:00 +0800

    优化pom

--- airbd-api/src/main/java/com/airboard/api/AirbdApiApplication.java
+++ airbd-api/src/main/java/com/airboard/api/AirbdApiApplication.java
@@ -1,9 +1,16 @@
 package com.airboard.api;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,10 +19,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.math.BigInteger;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
 @SpringBootApplication
-public class AirbdApiApplication {
+public class AirbdApiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbdApiApplication.class, args);
@@ -38,4 +48,25 @@ public class AirbdApiApplication {
                 .build();
     }
 
+    //将Spring MVC中默认的Jackson替换为fastjson
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverter() {
+        return new HttpMessageConverters(new FastJsonHttpMessageConverter());
+    }
+
+    //fastjson处理Long类型精度丢失问题
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter =
+                new FastJsonHttpMessageConverter();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
+    }
 }
--- airbd-web/src/main/java/com/airboard/web/AirbdWebApplication.java
+++ airbd-web/src/main/java/com/airboard/web/AirbdWebApplication.java
@@ -1,22 +1,59 @@
 package com.airboard.web;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.math.BigInteger;
+import java.util.List;
+
+@Configuration
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan("com.airboard")
 @MapperScan("com.airboard.core.dao")
 @EnableJpaRepositories("com.airboard")
 @EntityScan("com.airboard")
-public class AirbdWebApplication {
+public class AirbdWebApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(AirbdWebApplication.class, args);
     }
+
+
+    //将Spring MVC中默认的Jackson替换为fastjson
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverter() {
+        return new HttpMessageConverters(new FastJsonHttpMessageConverter());
+    }
+
+    //fastjson处理Long类型精度丢失问题
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter =
+                new FastJsonHttpMessageConverter();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
+    }
+
 }
