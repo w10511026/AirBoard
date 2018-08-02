bbbc66e0015aba36e44e259725af626bc7b661f2
shuo.wang@ksudi.com <Wangshuo>
2018-08-02 21:37:01 +0800

    更新

--- airbd-eureka/src/main/java/com/airboard/eureka/AirbdEurekaApplication.java
+++ airbd-eureka/src/main/java/com/airboard/eureka/AirbdEurekaApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAdminServer
+
 @Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
 @EnableEurekaServer
-@SpringBootApplication
 public class AirbdEurekaApplication {
 
     public static void main(String[] args) {
