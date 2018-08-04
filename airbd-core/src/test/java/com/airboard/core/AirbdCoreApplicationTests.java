6ed70616745db55af293d3025f6b24c0ec0d881f
shuo.wang@ksudi.com <Wangshuo>
2018-08-04 15:45:50 +0800

    更新

--- airbd-core/src/test/java/com/airboard/core/AirbdCoreApplicationTests.java
+++ airbd-core/src/test/java/com/airboard/core/AirbdCoreApplicationTests.java
@@ -1,26 +1,16 @@
 package com.airboard.core;
 
-import com.airboard.core.dao.system.SysUserMapper;
-import com.airboard.core.model.system.SysUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AirbdCoreApplicationTests {
 
-    @Autowired
-    SysUserMapper sysUserMapper;
-
     @Test
     public void contextLoads() {
-        List<SysUser> all = sysUserMapper.findAll();
-        System.out.println(all);
     }
 
 }
