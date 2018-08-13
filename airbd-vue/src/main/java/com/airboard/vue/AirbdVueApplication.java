cd2b196ff50f3c144d692fb029d91704e188841b
shuo.wang@ksudi.com <Wangshuo>
2018-08-13 21:45:07 +0800

    更新

---
+++ airbd-vue/src/main/java/com/airboard/vue/AirbdVueApplication.java
@@ -0,0 +1,12 @@
+package com.airboard.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbdVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbdVueApplication.class, args);
+    }
+}
