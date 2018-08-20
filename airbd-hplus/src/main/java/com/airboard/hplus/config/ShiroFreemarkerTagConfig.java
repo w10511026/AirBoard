51ca98cd6fc3169409042d8b3abec198f64144d3
Wangshuo <10511026@qq.com>
2018-08-20 23:07:43 +0800

    update

--- airbd-hplus/src/main/java/com/airboard/hplus/config/ShiroFreemarkerTagConfig.java
+++ airbd-hplus/src/main/java/com/airboard/hplus/config/ShiroFreemarkerTagConfig.java
@@ -2,8 +2,10 @@ package com.airboard.hplus.config;
 
 import com.jagregory.shiro.freemarker.ShiroTags;
 import freemarker.template.Configuration;
+import freemarker.template.TemplateModelException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,11 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShiroFreemarkerTagConfig implements InitializingBean {
 
+    @Value("${server.servlet.server-path}")
+    private String serverPath;
+
     @Autowired
     private Configuration configuration;
 
     @Override
-    public void afterPropertiesSet() {
+    public void afterPropertiesSet() throws TemplateModelException {
         configuration.setSharedVariable("shiro", new ShiroTags());
+        configuration.setSharedVariable("ctxPath", serverPath);
     }
 }
